49b4b89544e5d3d695d226bb30b92f407fa209bc
Heath Logan Campbell <github@heathlogancampbell.com>
2020-08-01 14:49:41 +1200

    add packets PlayerInfo and NamedEntitySpawn

---
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/PacketPlayOutNamedEntitySpawn.java
@@ -0,0 +1,63 @@
+package net.minecraft.server;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import net.minecraft.server.DataWatcher.WatchableObject;
+
+public class PacketPlayOutNamedEntitySpawn implements Packet<PacketListenerPlayOut> {
+    private int a; public int getEntityId() { return this.a; } public void setEntityId(int id) { this.a = id; }
+    private UUID b; public UUID getUUID() { return this.b; } public void setUUID(UUID uuid) { this.b = uuid; }
+    private int c; public int getRawX() { return this.c; }
+    private int d; public int getRawY() { return this.d; }
+    private int e; public int getRawZ() { return this.e; }
+    private byte f; public byte getRawPitch() { return this.f; }
+    private byte g; public byte getRawYaw() { return this.g; }
+    private int h; public int getData() { return this.h; }
+    private DataWatcher i;
+    private List<WatchableObject> j;
+
+    public PacketPlayOutNamedEntitySpawn() {
+    }
+
+    public PacketPlayOutNamedEntitySpawn(EntityHuman var1) {
+        this.a = var1.getId();
+        this.b = var1.getProfile().getId();
+        this.c = MathHelper.floor(var1.locX * 32.0D);
+        this.d = MathHelper.floor(var1.locY * 32.0D);
+        this.e = MathHelper.floor(var1.locZ * 32.0D);
+        this.f = (byte)((int)(var1.yaw * 256.0F / 360.0F));
+        this.g = (byte)((int)(var1.pitch * 256.0F / 360.0F));
+        ItemStack var2 = var1.inventory.getItemInHand();
+        this.h = var2 == null ? 0 : Item.getId(var2.getItem());
+        this.i = var1.getDataWatcher();
+    }
+
+    public void a(PacketDataSerializer var1) throws IOException {
+        this.a = var1.e();
+        this.b = var1.g();
+        this.c = var1.readInt();
+        this.d = var1.readInt();
+        this.e = var1.readInt();
+        this.f = var1.readByte();
+        this.g = var1.readByte();
+        this.h = var1.readShort();
+        this.j = DataWatcher.b(var1);
+    }
+
+    public void b(PacketDataSerializer var1) throws IOException {
+        var1.b(this.a);
+        var1.a(this.b);
+        var1.writeInt(this.c);
+        var1.writeInt(this.d);
+        var1.writeInt(this.e);
+        var1.writeByte(this.f);
+        var1.writeByte(this.g);
+        var1.writeShort(this.h);
+        this.i.a(var1);
+    }
+
+    public void a(PacketListenerPlayOut var1) {
+        var1.a(this);
+    }
+}
---
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/PacketPlayOutPlayerInfo.java
@@ -0,0 +1,226 @@
+package net.minecraft.server;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import net.minecraft.server.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.WorldSettings.EnumGamemode;
+
+public class PacketPlayOutPlayerInfo implements Packet<PacketListenerPlayOut> {
+    private PacketPlayOutPlayerInfo.EnumPlayerInfoAction a;
+    private final List<PacketPlayOutPlayerInfo.PlayerInfoData> b = Lists.newArrayList();
+
+    public PacketPlayOutPlayerInfo() {
+    }
+
+    public PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction var1, EntityPlayer... var2) {
+        this.a = var1;
+        EntityPlayer[] var3 = var2;
+        int var4 = var2.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            EntityPlayer var6 = var3[var5];
+            this.b.add(new PacketPlayOutPlayerInfo.PlayerInfoData(var6.getProfile(), var6.ping, var6.playerInteractManager.getGameMode(), var6.getPlayerListName()));
+        }
+
+    }
+
+    public PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction var1, Iterable<EntityPlayer> var2) {
+        this.a = var1;
+        Iterator var3 = var2.iterator();
+
+        while(var3.hasNext()) {
+            EntityPlayer var4 = (EntityPlayer)var3.next();
+            this.b.add(new PacketPlayOutPlayerInfo.PlayerInfoData(var4.getProfile(), var4.ping, var4.playerInteractManager.getGameMode(), var4.getPlayerListName()));
+        }
+
+    }
+
+    public PacketPlayOutPlayerInfo.EnumPlayerInfoAction getPlayerInfoAction()
+    {
+        return this.a;
+    }
+
+    public void setPlayerInfoAction(PacketPlayOutPlayerInfo.EnumPlayerInfoAction action)
+    {
+        this.a = action;
+    }
+
+    public List<PlayerInfoData> getPlayersInfo()
+    {
+        return b;
+    }
+
+    public void a(PacketDataSerializer var1) throws IOException {
+        this.a = (PacketPlayOutPlayerInfo.EnumPlayerInfoAction)var1.a(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.class);
+        int var2 = var1.e();
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            GameProfile var4 = null;
+            int var5 = 0;
+            EnumGamemode var6 = null;
+            IChatBaseComponent var7 = null;
+            switch(this.a) {
+            case ADD_PLAYER:
+                var4 = new GameProfile(var1.g(), var1.c(16));
+                int var8 = var1.e();
+                int var9 = 0;
+
+                for(; var9 < var8; ++var9) {
+                    String var10 = var1.c(32767);
+                    String var11 = var1.c(32767);
+                    if (var1.readBoolean()) {
+                        var4.getProperties().put(var10, new Property(var10, var11, var1.c(32767)));
+                    } else {
+                        var4.getProperties().put(var10, new Property(var10, var11));
+                    }
+                }
+
+                var6 = EnumGamemode.getById(var1.e());
+                var5 = var1.e();
+                if (var1.readBoolean()) {
+                    var7 = var1.d();
+                }
+                break;
+            case UPDATE_GAME_MODE:
+                var4 = new GameProfile(var1.g(), (String)null);
+                var6 = EnumGamemode.getById(var1.e());
+                break;
+            case UPDATE_LATENCY:
+                var4 = new GameProfile(var1.g(), (String)null);
+                var5 = var1.e();
+                break;
+            case UPDATE_DISPLAY_NAME:
+                var4 = new GameProfile(var1.g(), (String)null);
+                if (var1.readBoolean()) {
+                    var7 = var1.d();
+                }
+                break;
+            case REMOVE_PLAYER:
+                var4 = new GameProfile(var1.g(), (String)null);
+            }
+
+            this.b.add(new PacketPlayOutPlayerInfo.PlayerInfoData(var4, var5, var6, var7));
+        }
+
+    }
+
+    public void b(PacketDataSerializer var1) throws IOException {
+        var1.a(this.a);
+        var1.b(this.b.size());
+        Iterator var2 = this.b.iterator();
+
+        while(true) {
+            while(var2.hasNext()) {
+                PacketPlayOutPlayerInfo.PlayerInfoData var3 = (PacketPlayOutPlayerInfo.PlayerInfoData)var2.next();
+                switch(this.a) {
+                case ADD_PLAYER:
+                    var1.a(var3.a().getId());
+                    var1.a(var3.a().getName());
+                    var1.b(var3.a().getProperties().size());
+                    Iterator var4 = var3.a().getProperties().values().iterator();
+
+                    while(var4.hasNext()) {
+                        Property var5 = (Property)var4.next();
+                        var1.a(var5.getName());
+                        var1.a(var5.getValue());
+                        if (var5.hasSignature()) {
+                            var1.writeBoolean(true);
+                            var1.a(var5.getSignature());
+                        } else {
+                            var1.writeBoolean(false);
+                        }
+                    }
+
+                    var1.b(var3.c().getId());
+                    var1.b(var3.b());
+                    if (var3.d() == null) {
+                        var1.writeBoolean(false);
+                    } else {
+                        var1.writeBoolean(true);
+                        var1.a(var3.d());
+                    }
+                    break;
+                case UPDATE_GAME_MODE:
+                    var1.a(var3.a().getId());
+                    var1.b(var3.c().getId());
+                    break;
+                case UPDATE_LATENCY:
+                    var1.a(var3.a().getId());
+                    var1.b(var3.b());
+                    break;
+                case UPDATE_DISPLAY_NAME:
+                    var1.a(var3.a().getId());
+                    if (var3.d() == null) {
+                        var1.writeBoolean(false);
+                    } else {
+                        var1.writeBoolean(true);
+                        var1.a(var3.d());
+                    }
+                    break;
+                case REMOVE_PLAYER:
+                    var1.a(var3.a().getId());
+                }
+            }
+
+            return;
+        }
+    }
+
+    public void a(PacketListenerPlayOut var1) {
+        var1.a(this);
+    }
+
+    public String toString() {
+        return Objects.toStringHelper(this).add("action", this.a).add("entries", this.b).toString();
+    }
+
+    public class PlayerInfoData {
+        private final int b;
+        private final EnumGamemode c;
+        private final GameProfile d;
+        private final IChatBaseComponent e;
+
+        public PlayerInfoData(GameProfile var2, int var3, EnumGamemode var4, IChatBaseComponent var5) {
+            this.d = var2;
+            this.b = var3;
+            this.c = var4;
+            this.e = var5;
+        }
+
+        public GameProfile a() {
+            return this.d;
+        }
+
+        public int b() {
+            return this.b;
+        }
+
+        public EnumGamemode c() {
+            return this.c;
+        }
+
+        public IChatBaseComponent d() {
+            return this.e;
+        }
+
+        public String toString() {
+            return Objects.toStringHelper(this).add("latency", this.b).add("gameMode", this.c).add("profile", this.d).add("displayName", this.e == null ? null : ChatSerializer.a(this.e)).toString();
+        }
+    }
+
+    public static enum EnumPlayerInfoAction {
+        ADD_PLAYER,
+        UPDATE_GAME_MODE,
+        UPDATE_LATENCY,
+        UPDATE_DISPLAY_NAME,
+        REMOVE_PLAYER;
+
+        private EnumPlayerInfoAction() {
+        }
+    }
+}
