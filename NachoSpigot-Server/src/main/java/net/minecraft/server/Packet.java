a088e1387637ce8c1ff2caf658c17df97cafcb64
Lucaskyy <lucabrio2005@gmail.com>
2021-04-08 17:31:38 +0200

    i am so stupid
    how did i think this was gonna work?

--- NachoSpigot-Server/src/main/java/net/minecraft/server/Packet.java
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/Packet.java
@@ -6,18 +6,18 @@ public interface Packet<T extends PacketListener> {
     /**
      * Reads the raw packet data from the data stream.
      */
-    void readPacketData(PacketDataSerializer serializer) throws IOException; // Nacho - OBFHELPER
+    default void readPacketData(PacketDataSerializer serializer) throws IOException {} // Nacho - OBFHELPER
     default void a(PacketDataSerializer serializer) throws IOException { readPacketData(serializer); }
 
     /**
      * Writes the raw packet data to the data stream.
      */
-    void writePacketData(PacketDataSerializer serializer) throws IOException; // Nacho - OBFHELPER
+    default void writePacketData(PacketDataSerializer serializer) throws IOException {}; // Nacho - OBFHELPER
     default void b(PacketDataSerializer serializer) throws IOException { writePacketData(serializer); }
 
     /**
      * Passes this packet on to the NetHandler for processing.
      */
-    void processPacket(T t0); // Nacho - OBFHELPER
+    default void processPacket(T t0) {}; // Nacho - OBFHELPER
     default void a(T handler) { processPacket(handler); }
 }
