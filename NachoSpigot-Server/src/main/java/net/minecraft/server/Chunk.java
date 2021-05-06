76966334a9e509f45a3272d32dc8d85de5d41957
LewUwU <72635645+LewUwU@users.noreply.github.com>
2021-05-06 08:42:33 -0400

    [InsanePaper-269] Cache Chunk Coordinations
    Prevent the creation of multiple Chunk Coordinations by calling the Chunk#j method

--- NachoSpigot-Server/src/main/java/net/minecraft/server/Chunk.java
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/Chunk.java
@@ -133,6 +133,8 @@ public class Chunk {
     public PacketPlayOutMapChunkBulk cachedPacket;
     public PacketPlayOutMapChunk cachedEmptyPacket;
 
+    private final ChunkCoordIntPair chunkCoords;
+    
     public Chunk(World world, int i, int j) {
         this.sections = new ChunkSection[16];
         this.e = new byte[256];
@@ -158,6 +160,7 @@ public class Chunk {
         if (!(this instanceof EmptyChunk)) {
             this.bukkitChunk = new org.bukkit.craftbukkit.CraftChunk(this);
         }
+        this.chunkCoords = new ChunkCoordIntPair(this.locX, this.locZ);
     }
 
     public org.bukkit.Chunk bukkitChunk;
@@ -1329,7 +1332,7 @@ public class Chunk {
     }
 
     public ChunkCoordIntPair j() {
-        return new ChunkCoordIntPair(this.locX, this.locZ);
+        return this.chunkCoords;
     }
 
     public boolean c(int i, int j) {
