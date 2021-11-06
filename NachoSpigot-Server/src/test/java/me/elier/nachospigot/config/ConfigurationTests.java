0ccc88393d75dc8e1108f00c44b164172c5d2ffc
Elier <elier.lopezg@hotmail.com>
2021-11-06 06:22:59 -0400

    Show errors in tests (#219)

--- NachoSpigot-Server/src/test/java/me/elier/nachospigot/config/ConfigurationTests.java
+++ NachoSpigot-Server/src/test/java/me/elier/nachospigot/config/ConfigurationTests.java
@@ -2,6 +2,7 @@ package me.elier.nachospigot.config;
 
 import dev.cobblesword.nachospigot.OldNachoConfig;
 import dev.cobblesword.nachospigot.commons.FileUtils;
+import org.bukkit.support.DummyServer;
 import org.junit.Test;
 
 import java.io.File;
@@ -12,12 +13,14 @@ public class ConfigurationTests {
 
     @Test
     public void migrateFullConfig() {
+        DummyServer.setup();
         FileUtils.toFile(new OldNachoConfig(), new File("nacho.json"));
         NachoConfig.init(new File("nacho.yml"));
     }
 
     @Test
     public void migratePartialModifiedConfig() throws IOException {
+        DummyServer.setup();
         File file = new File("nacho.json");
         file.createNewFile();
         FileWriter writer = new FileWriter(file);
@@ -29,12 +32,14 @@ public class ConfigurationTests {
 
     @Test
     public void migrateEmptyConfig() throws IOException {
+        DummyServer.setup();
         new File("nacho.json").createNewFile();
         NachoConfig.init(new File("nacho.yml"));
     }
 
     @Test
     public void loadConfig() {
+        DummyServer.setup();
         NachoConfig.init(new File("nacho.yml"));
     }
 }
