8f2650f93674cf4e0a31b45ca4e1b135d5382048
sadcenter <55924469+sadcenter@users.noreply.github.com>
2021-11-11 09:04:26 -0500

    Revert "Show errors in tests (#219)"
    This reverts commit 0ccc88393d75dc8e1108f00c44b164172c5d2ffc.

--- NachoSpigot-Server/src/test/java/me/elier/nachospigot/config/ConfigurationTests.java
+++ NachoSpigot-Server/src/test/java/me/elier/nachospigot/config/ConfigurationTests.java
@@ -2,7 +2,6 @@ package me.elier.nachospigot.config;
 
 import dev.cobblesword.nachospigot.OldNachoConfig;
 import dev.cobblesword.nachospigot.commons.FileUtils;
-import org.bukkit.support.DummyServer;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,14 +12,12 @@ public class ConfigurationTests {
 
     @Test
     public void migrateFullConfig() {
-        DummyServer.setup();
         FileUtils.toFile(new OldNachoConfig(), new File("nacho.json"));
         NachoConfig.init(new File("nacho.yml"));
     }
 
     @Test
     public void migratePartialModifiedConfig() throws IOException {
-        DummyServer.setup();
         File file = new File("nacho.json");
         file.createNewFile();
         FileWriter writer = new FileWriter(file);
@@ -32,14 +29,12 @@ public class ConfigurationTests {
 
     @Test
     public void migrateEmptyConfig() throws IOException {
-        DummyServer.setup();
         new File("nacho.json").createNewFile();
         NachoConfig.init(new File("nacho.yml"));
     }
 
     @Test
     public void loadConfig() {
-        DummyServer.setup();
         NachoConfig.init(new File("nacho.yml"));
     }
 }
