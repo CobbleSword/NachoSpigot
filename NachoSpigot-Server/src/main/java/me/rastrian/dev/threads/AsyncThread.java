a2828cc992050662be1fe3f3df6efb2736e126a3
Rastrian <me@rastrian.dev>
2021-11-17 10:18:10 -0300

    feat(TPS): Added custom TPS tracker to Async Threads

--- NachoSpigot-Server/src/main/java/me/rastrian/dev/threads/AsyncThread.java
+++ NachoSpigot-Server/src/main/java/me/rastrian/dev/threads/AsyncThread.java
@@ -8,15 +8,24 @@ import me.rastrian.dev.threads.netty.Spigot404Write;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.NetworkManager;
 import net.minecraft.server.Packet;
 import net.openhft.affinity.AffinityLock;
 import net.openhft.affinity.AffinityStrategies;
 
 public abstract class AsyncThread {
-    private static final long TICK_TIME = 1000000000 / NachoConfig.combatThreadTPS;
-    protected Queue<Runnable> packets = new ConcurrentLinkedQueue<Runnable>();
+    private static final long SEC_IN_NANO = 1000000000;
+    private static final int TPS = NachoConfig.combatThreadTPS;
+    private static final long TICK_TIME = SEC_IN_NANO / TPS;
     
+    private int currentTick = 0;
+    private final RollingAverage tps1 = new RollingAverage(60);
+    private final RollingAverage tps5 = new RollingAverage(300);
+    private final RollingAverage tps15 = new RollingAverage(900);
+
+    protected Queue<Runnable> packets = new ConcurrentLinkedQueue<Runnable>();
     private boolean running = true;
     private Thread t;
 
@@ -54,12 +63,28 @@ public abstract class AsyncThread {
                 continue;
             }
             catchupTime = Math.min(1000000000L, Math.abs(wait));
+            if ( ++this.currentTick % TPS == 0 )
+            {
+                final long diff = curTime - lastTick;
+                double currentTps = 1E9 / diff * TPS;
+                tps1.add(currentTps, diff);
+                tps5.add(currentTps, diff);
+                tps15.add(currentTps, diff);
+            }
             this.run();
             lastTick = curTime;
         }
     }
 
     public abstract void run();
+
+    public double[] getTPS() {
+        return new double[] {
+            this.tps1.getAverage(),
+            this.tps5.getAverage(),
+            this.tps15.getAverage()
+        };
+    }
 
     public void addPacket(final Packet<?>  packet, final NetworkManager manager, final GenericFutureListener<? extends Future<? super Void>>[] agenericfuturelistener) {
         this.packets.add(new Runnable(){
@@ -90,6 +115,43 @@ public abstract class AsyncThread {
 
         public Channel getChannel() {
             return this.channel;
+        }
+    }
+
+    public static class RollingAverage {
+        private final int size;
+        private long time;
+        private double total;
+        private int index = 0;
+        private final double[] samples;
+        private final long[] times;
+
+        RollingAverage(int size) {
+            this.size = size;
+            this.time = size * SEC_IN_NANO;
+            this.total = TPS * SEC_IN_NANO * size;
+            this.samples = new double[size];
+            this.times = new long[size];
+            for (int i = 0; i < size; i++) {
+                this.samples[i] = TPS;
+                this.times[i] = SEC_IN_NANO;
+            }
+        }
+
+        public void add(double x, long t) {
+            time -= times[index];
+            total -= samples[index] * times[index];
+            samples[index] = x;
+            times[index] = t;
+            time += t;
+            total += x * t;
+            if (++index == size) {
+                index = 0;
+            }
+        }
+
+        public double getAverage() {
+            return total / time;
         }
     }
 }
--- NachoSpigot-Server/src/main/java/org/spigotmc/TicksPerSecondCommand.java
+++ NachoSpigot-Server/src/main/java/org/spigotmc/TicksPerSecondCommand.java
@@ -4,6 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import dev.cobblesword.nachospigot.Nacho;
+import me.elier.nachospigot.config.NachoConfig;
+
 public class TicksPerSecondCommand extends Command
 {
 
@@ -26,18 +29,49 @@ public class TicksPerSecondCommand extends Command
         // PaperSpigot start - Further improve tick handling
         double[] tps = org.bukkit.Bukkit.spigot().getTPS();
         String[] tpsAvg = new String[tps.length];
-
-         for ( int i = 0; i < tps.length; i++) {
-             tpsAvg[i] = format( tps[i] );
+        
+        for ( int i = 0; i < tps.length; i++) {
+            tpsAvg[i] = format(tps[i], true);
         }
-        sender.sendMessage( ChatColor.GOLD + "TPS from last 1m, 5m, 15m: " + org.apache.commons.lang.StringUtils.join(tpsAvg, ", "));
+        
+        sender.sendMessage( ChatColor.GOLD + "General TPS from last 1m, 5m, 15m: " + formatStringTPS(tpsAvg));
+        
+        if (NachoConfig.asyncHitDetection) {
+            double[] hitDetectionTPS = Nacho.hitDetectionThread.getTPS();
+            tpsAvg = new String[hitDetectionTPS.length];
+            
+            for ( int i = 0; i < hitDetectionTPS.length; i++) {
+                tpsAvg[i] = format(hitDetectionTPS[i], false);
+            }
+            
+            sender.sendMessage( ChatColor.GOLD + "Hit Detection TPS from last 1m, 5m, 15m: " + formatStringTPS(tpsAvg));
+        }
+        
+        if (NachoConfig.asyncKnockback) {
+            double[] knockbackTPS = Nacho.knockbackThread.getTPS();
+            tpsAvg = new String[knockbackTPS.length];
+
+            for ( int i = 0; i < knockbackTPS.length; i++) {
+                tpsAvg[i] = format(knockbackTPS[i], false);
+            }
+            sender.sendMessage( ChatColor.GOLD + "Knockback TPS from last 1m, 5m, 15m: " + formatStringTPS(tpsAvg));
+        }
+
         sender.sendMessage(ChatColor.GOLD + "Current Memory Usage: " + ChatColor.GREEN + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)) + "/" + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " mb (Max: " + (Runtime.getRuntime().maxMemory() / (1024 * 1024)) + " mb)");
         return true;
     }
 
-    private static String format(double tps) // PaperSpigot - made static
+    private static String formatStringTPS(String[] tpsAvg) {
+        return org.apache.commons.lang.StringUtils.join(tpsAvg, ", ");
+    }
+
+    private static String format(double tps, boolean limit) // PaperSpigot - made static
     {
-        return ( ( tps > 18.0 ) ? ChatColor.GREEN : ( tps > 16.0 ) ? ChatColor.YELLOW : ChatColor.RED ).toString()
+        if (limit) {
+            return ( ( tps > 18.0 ) ? ChatColor.GREEN : ( tps > 16.0 ) ? ChatColor.YELLOW : ChatColor.RED ).toString()
                 + ( ( tps > 20.0 ) ? "*" : "" ) + Math.min( Math.round( tps * 100.0 ) / 100.0, 20.0 );
+        }
+        return (( tps > ((90.0 * tps) / 100.0) ) ? ChatColor.GREEN : ( tps > ((80.0 * tps) / 100.0) ) ? ChatColor.YELLOW : ChatColor.RED ).toString()
+                + Math.min( Math.round( tps * 100.0 ) / 100.0, NachoConfig.combatThreadTPS );
     }
 }
