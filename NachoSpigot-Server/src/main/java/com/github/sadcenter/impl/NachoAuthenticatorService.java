a777e517c04c7a5fa3b80858735d88ddb98f2f91
sadcenter <kacper.krzychala@gmail.com>
2021-11-18 10:59:59 +0100

    Improvements, Ignore case in map, 1 save/tick, fixed code-style, code clean-up

--- NachoSpigot-Server/src/main/java/com/github/sadcenter/impl/NachoAuthenticatorService.java
+++ NachoSpigot-Server/src/main/java/com/github/sadcenter/impl/NachoAuthenticatorService.java
@@ -1,32 +1,23 @@
 package com.github.sadcenter.impl;
 
-import com.github.sadcenter.core.AsyncHttpAuthenticator;
 import com.github.sadcenter.core.NachoAuthenticator;
-import com.github.sadcenter.impl.repo.NachoGameProfileRepository;
-import com.github.sadcenter.impl.session.NachoSessionService;
 import com.google.gson.*;
-import com.mojang.authlib.*;
-import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import com.mojang.authlib.yggdrasil.response.ProfileSearchResultsResponse;
-import com.mojang.util.UUIDTypeAdapter;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.net.Proxy;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class NachoAuthenticatorService extends NachoAuthenticator {
 
-    private final Gson gson = new GsonBuilder()
-            .registerTypeAdapter(GameProfile.class, new GameProfileSerializer())
-            .registerTypeAdapter(PropertyMap.class, new PropertyMap.Serializer())
-            .registerTypeAdapter(UUID.class, new UUIDTypeAdapter())
-            .registerTypeAdapter(ProfileSearchResultsResponse.class, new ProfileSearchResultsResponse.Serializer())
+    public static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(GameProfile.class, new NachoAuthenticatorService.GameProfileSerializer())
             .create();
 
     @Override
@@ -34,11 +25,11 @@ public class NachoAuthenticatorService extends NachoAuthenticator {
         return CompletableFuture.supplyAsync(() -> {
             String result = null;
             try {
-                result = super.fetchGet(NachoAuthenticator.url(url));
+                result = super.fetchGet(url(url));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return gson.fromJson(result, type);
+            return GSON.fromJson(result, type);
         });
     }
 
@@ -46,7 +37,7 @@ public class NachoAuthenticatorService extends NachoAuthenticator {
     public <T> CompletableFuture<T> post(String url, Object content, Class<T> type) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                return gson.fromJson(super.fetchPost(NachoAuthenticator.url(url), gson.toJson(content)), type);
+                return GSON.fromJson(super.fetchPost(url(url), GSON.toJson(content)), type);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -55,8 +46,35 @@ public class NachoAuthenticatorService extends NachoAuthenticator {
         });
     }
 
-    private static class GameProfileSerializer implements JsonSerializer<GameProfile>, JsonDeserializer<GameProfile>
-    {
+    public static URL url(String url) {
+        try {
+            return new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String json(Map<?, ?> map) {
+        return GSON.toJson(map);
+    }
+
+    public static String query(Map<?, ?> map) {
+        StringBuilder builder = new StringBuilder();
+        AtomicBoolean firstPair = new AtomicBoolean(true);
+
+        map.forEach((a, b) -> {
+            builder.append((firstPair.get() ? "?" : "&") + a + "=" + b);
+
+            firstPair.set(false);
+        });
+
+        return builder.toString();
+    }
+
+    public static class GameProfileSerializer implements JsonDeserializer<GameProfile> {
+        @Override
         public GameProfile deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
             JsonObject jsonObject = json.getAsJsonObject();
             UUID id = jsonObject.has("uuid") ? UUID.fromString(jsonObject.getAsJsonPrimitive("uuid").getAsString()) : null;
@@ -66,25 +84,11 @@ public class NachoAuthenticatorService extends NachoAuthenticator {
 
             if (jsonObject.has("textures")) {
                 JsonObject propertyJson = jsonObject.get("textures").getAsJsonObject().get("raw").getAsJsonObject();
-
                 Property property = new Property("textures", propertyJson.getAsJsonPrimitive("value").getAsString());
-
                 gameProfile.getProperties().put("textures", property);
             }
 
             return gameProfile;
-        }
-
-        public JsonElement serialize(final GameProfile src, final Type typeOfSrc, final JsonSerializationContext context) {
-            final JsonObject result = new JsonObject();
-            if (src.getId() != null) {
-                result.add("id", context.serialize((Object)src.getId()));
-            }
-            if (src.getName() != null) {
-                result.addProperty("name", src.getName());
-            }
-
-            return result;
         }
     }
 }
