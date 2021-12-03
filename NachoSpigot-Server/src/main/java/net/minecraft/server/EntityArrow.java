35f2a0971e4595da0badd550c6f99f49885c558d
Tofpu <47629321+Tofpu@users.noreply.github.com>
2021-12-03 23:43:29 +0200

    arrow not having a defined shooter fix
    this fixes the arrow from throwing an exception when the shooter is null

--- NachoSpigot-Server/src/main/java/net/minecraft/server/EntityArrow.java
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/EntityArrow.java
@@ -323,7 +323,7 @@ public class EntityArrow extends Entity implements IProjectile {
                     this.locX -= this.motX / (double) f1 * 0.05000000074505806D;
                     this.locY -= this.motY / (double) f1 * 0.05000000074505806D;
                     this.locZ -= this.motZ / (double) f1 * 0.05000000074505806D;
-                    world.makeSound(this.shooter,"random.bowhit", 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
+                    world.makeSound(d, e, f,"random.bowhit", 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
                     this.inGround = true;
                     this.shake = 7;
                     this.setCritical(false);
