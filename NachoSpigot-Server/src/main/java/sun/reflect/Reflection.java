f422e973f7090533a761e6b76be833d5744f9c9c
Lucaskyy <contact@sculas.xyz>
2021-12-10 18:37:58 +0100

    Trying this

--- NachoSpigot-Server/src/main/java/sun/reflect/Reflection.java
+++
@@ -1,8 +0,0 @@
-package sun.reflect;
-
-public class Reflection {
-    public static Class<?> getCallerClass(int n) {
-        StackTraceElement[] elements = new Throwable().getStackTrace();
-        return elements[n].getClass();
-    }
-}
