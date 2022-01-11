7fa836a6968b167688fce5eb168982072b3aa6ca
Elierrr <71361901+Elierrr@users.noreply.github.com>
2022-01-11 16:09:56 -0500

    Fix Java 9-15

--- NachoSpigot-Server/src/main/java/dev/cobblesword/nachospigot/GuavaInjector.java
+++ NachoSpigot-Server/src/main/java/dev/cobblesword/nachospigot/GuavaInjector.java
@@ -1,7 +1,8 @@
 package dev.cobblesword.nachospigot;
 
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.PluginClassLoader;
 import org.objectweb.asm.*;
 
 import java.lang.reflect.Method;
@@ -11,17 +12,19 @@ import static org.objectweb.asm.Opcodes.*;
 
 public class GuavaInjector {
 
-    private static byte[] moreExecutorsBytes;
-
-    private static void defineClass(String name, byte[] b) {
-        try {
-            // GuavaInjector.class.getClassLoader().defineCLass(name, b, 0, b.length);
-            Method defineClass = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
-            defineClass.setAccessible(true);
-            defineClass.invoke(GuavaInjector.class.getClassLoader(), name, b, 0, b.length);
-        } catch (Exception e) {
-            Bukkit.getLogger().log(Level.SEVERE, "Exception while defining class", e);
+    @SuppressWarnings({"SameParameterValue", "JavaReflectionInvocation"})
+    private static void defineClass(String name, byte[] b) throws ReflectiveOperationException {
+        if (SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_9)) {
+            Class<?> moduleClazz = Class.forName("java.lang.Module");
+            Method getModule = Class.class.getDeclaredMethod("getModule");
+            Method addOpens = moduleClazz.getDeclaredMethod("addOpens", String.class, moduleClazz);
+            // Module.class.getModule().addOpens("java.lang", GuavaInjector.class.getModule);
+            addOpens.invoke(getModule.invoke(moduleClazz), "java.lang", getModule.invoke(GuavaInjector.class));
         }
+        // GuavaInjector.class.getClassLoader().defineCLass(name, b, 0, b.length);
+        Method defineClass = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
+        defineClass.setAccessible(true);
+        defineClass.invoke(GuavaInjector.class.getClassLoader(), name, b, 0, b.length);
     }
 
     static {
@@ -30,11 +33,10 @@ public class GuavaInjector {
             ClassReader cr = new ClassReader("com.google.common.util.concurrent.MoreExecutors");
             ClassWriter cw = new ClassWriter(cr, 0);
             cr.accept(new MoreExecutorsVisitor(cw), 0);
-            moreExecutorsBytes = cw.toByteArray();
+            defineClass("com.google.common.util.concurrent.MoreExecutors", cw.toByteArray());
         } catch (Exception e) {
-            Bukkit.getLogger().log(Level.SEVERE, "Exception creating method com.google.common.util.concurrent.MoreExecutors.sameThreadExecutor", e);
+            Bukkit.getLogger().log(Level.SEVERE, "Error creating method com.google.common.util.concurrent.MoreExecutors.sameThreadExecutor", e);
         }
-        defineClass("com.google.common.util.concurrent.MoreExecutors", moreExecutorsBytes);
     }
 
     public static void load() {}
