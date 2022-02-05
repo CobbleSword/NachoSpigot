81d4e9fa5bfa2ef2f942b39524e2b04dc082b0ff
Elier <71361901+Elierrr@users.noreply.github.com>
2022-02-05 18:35:46 -0500

    Create Keyed.java

---
+++ NachoSpigot-API/src/main/java/org/bukkit/Keyed.java
@@ -0,0 +1,29 @@
+package org.bukkit;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents an object which has a {@link NamespacedKey} attached to it.
+ */
+public interface Keyed extends net.kyori.adventure.key.Keyed { // Paper -- extend Adventure Keyed
+
+    /**
+     * Return the namespaced identifier for this object.
+     *
+     * @return this object's key
+     */
+    @NotNull
+    NamespacedKey getKey();
+  
+    // Paper start
+    /**
+     * Returns the unique identifier for this object.
+     *
+     * @return this object's key
+     */
+    @Override
+    default net.kyori.adventure.key.@NotNull Key key() {
+        return this.getKey();
+    }
+    // Paper end
+}
