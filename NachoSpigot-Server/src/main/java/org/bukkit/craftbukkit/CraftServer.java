d9a821621e08124159339764743f3b71dba36c9e
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2022-02-19 17:08:16 +0100

    add SportPaper #206 Remove the world before nullifying chunkLoader & chunkProvider

--- NachoSpigot-Server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ NachoSpigot-Server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -1038,7 +1038,10 @@ public final class CraftServer implements Server {
         if (e.isCancelled()) {
             return false;
         }
-
+		
+		worlds.remove(world.getName().toLowerCase());
+        console.worlds.remove(handle);
+		
         if (save) {
             try {
                 handle.save(true, null);
@@ -1063,9 +1066,6 @@ public final class CraftServer implements Server {
             chunkProviderServer.chunkProvider = null;
             chunkProviderServer.chunks.clear();
         }
-
-        worlds.remove(world.getName().toLowerCase());
-        console.worlds.remove(handle);
 
         // KigPaper start - fix memory leak
         CraftingManager craftingManager = CraftingManager.getInstance();
