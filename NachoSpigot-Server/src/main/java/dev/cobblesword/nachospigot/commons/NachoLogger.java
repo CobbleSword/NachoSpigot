cb7d1208eb1146b89cb48ccc459e720121f45714
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2022-03-06 18:43:29 +0100

    Delete NachoLogger.java

--- NachoSpigot-Server/src/main/java/dev/cobblesword/nachospigot/commons/NachoLogger.java
+++
@@ -1,8 +0,0 @@
-package dev.cobblesword.nachospigot.commons;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class NachoLogger {
-    public static final Logger LOGGER = LogManager.getLogger(NachoLogger.class);
-}
