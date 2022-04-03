fe6d3937c638be5d98fe3cd70d51b550a4431d7c
128931 <101839340+128931@users.noreply.github.com>
2022-04-02 19:07:44 -0700

    Update dependencies
    Not too sure if this would break stuff but I'm pretty sure it shouldn't as I checked the updates of these depends and majority are just bug fixes/quality of life changes

--- NachoSpigot-API/src/main/java/org/bukkit/configuration/file/YamlConfiguration.java
+++ NachoSpigot-API/src/main/java/org/bukkit/configuration/file/YamlConfiguration.java
@@ -1,13 +1,5 @@
 package org.bukkit.configuration.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.util.Map;
-import java.util.logging.Level;
-
 import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
@@ -17,6 +9,10 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.error.YAMLException;
 import org.yaml.snakeyaml.representer.Representer;
+
+import java.io.*;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
  * An implementation of {@link Configuration} which saves all files in Yaml.
@@ -52,7 +48,7 @@ public class YamlConfiguration extends FileConfiguration {
 
         Map<?, ?> input;
         try {
-            input = (Map<?, ?>) yaml.load(contents);
+            input = yaml.load(contents);
         } catch (YAMLException e) {
             throw new InvalidConfigurationException(e);
         } catch (ClassCastException e) {
