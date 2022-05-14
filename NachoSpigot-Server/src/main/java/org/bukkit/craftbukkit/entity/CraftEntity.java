439690999e1f7df73b4515d5975783cc0a2df3dd
Packsolite <packsolite@gmail.com>
2022-05-14 17:40:53 +0200

    Use 'Nacho start' instead of paper

--- NachoSpigot-Server/src/main/java/org/bukkit/craftbukkit/entity/CraftEntity.java
+++ NachoSpigot-Server/src/main/java/org/bukkit/craftbukkit/entity/CraftEntity.java
@@ -214,9 +214,9 @@ public abstract class CraftEntity implements org.bukkit.entity.Entity {
         }
         // Paper end
         
-        // Paper start - don't allow plugins to set problematic velocity vectors
+        // Nacho start - don't allow plugins to set problematic velocity vectors
         Preconditions.checkArgument(vel.getX() == vel.getX() && vel.getY() == vel.getY() && vel.getZ() == vel.getZ(), "Illegal velocity vector: " + vel);
-        // Paper end
+        // Nacho end
 
         entity.motX = vel.getX();
         entity.motY = vel.getY();
